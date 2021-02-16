acee58fd162de3d05a5ddc705be5b6ae553a54eb
arkticman <erick.meyer@protonmail.com>
2021-02-15 20:53:23 -0800

    disable LED stuff

--- snazzy/config.h
+++ snazzy/config.h
@@ -5,7 +5,7 @@
 #   define RGB_DISABLE_WHEN_USB_SUSPENDED true // turn off effects when suspended
 #   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 // #   define RGB_MATRIX_LED_PROCESS_LIMIT (DRIVER_LED_TOTAL + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
-// #   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+#   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
 #    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash. 
 #    define RGB_MATRIX_HUE_STEP 8
 #    define RGB_MATRIX_SAT_STEP 8
@@ -19,22 +19,22 @@
 // #   define DISABLE_RGB_MATRIX_BREATHING
 // #   define DISABLE_RGB_MATRIX_CYCLE_ALL
 // #   define DISABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
-// #   define DISABLE_RGB_MATRIX_CYCLE_UP_DOWN
-// #   define DISABLE_RGB_MATRIX_CYCLE_OUT_IN
-// #   define DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+#   define DISABLE_RGB_MATRIX_CYCLE_UP_DOWN
+#   define DISABLE_RGB_MATRIX_CYCLE_OUT_IN
+#   define DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
 // #   define DISABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
 // #   define DISABLE_RGB_MATRIX_DUAL_BEACON
 // #   define DISABLE_RGB_MATRIX_RAINBOW_BEACON
 // #   define DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS
 // #   define DISABLE_RGB_MATRIX_RAINDROPS
 // #   define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
-// #   define DISABLE_RGB_MATRIX_TYPING_HEATMAP
-// #   define DISABLE_RGB_MATRIX_DIGITAL_RAIN
-// #   define DISABLE_RGB_MATRIX_SOLID_REACTIVE
-// #   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
-// #   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
-// #   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
-// #   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+#   define DISABLE_RGB_MATRIX_TYPING_HEATMAP
+#   define DISABLE_RGB_MATRIX_DIGITAL_RAIN
+#   define DISABLE_RGB_MATRIX_SOLID_REACTIVE
+#   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+#   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+#   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+#   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
 // #   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
 // #   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
 // #   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
